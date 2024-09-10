62d51fd33fe2c53e1e214fb3a97dbd942fa2eacd
Anupam Yadav <anupamy571@gmail.com>
2024-09-10 13:18:22 +0530

    Error resolved of dotenv

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 import express from "express";
-import router from "./router/fileRouter.js"; // Ensure correct import
+import router from "./router/fileRouter.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+const PORT = process.env.PORT || 3000;
 dotenv.config();
-
-const PORT = process.env.PORT || 3000; // Use environment variable or default to 8080
 const app = express();
 
-// Middleware
 app.use(express.json());
 
 mongoose
-  .connect(`${process.env.DATABASE_URI}/${process.env.DB_NAME}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
+  .connect(`${process.env.DATABASE_URI}${process.env.DB_NAME}`)
+  .then(() => console.log("Connected to database."))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Use router
-// Use router
 app.use("/", router);
 
-console.log("after router");
-
-// Start server
-app
-  .listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-    console.log(`Server is running at http://localhost:${PORT}`);
-  })
-  .on("error", (error) => {
-    console.error("Error starting server:", error);
-  });
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
